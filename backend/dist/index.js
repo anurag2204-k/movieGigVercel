cef8c76eb73dd4623c89afa029c8d984103efb31
Anurag Khobragae <anuragk2204@gmail.com>
2024-12-18 00:15:15 +0530

    ff

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173', // Frontend UR
+    origin: "https://movie-gig-vercel.vercel.app/", // Frontend URLs
     credentials: true, // Allow sending cookies
 }));
 app.get('/', (req, res) => {
