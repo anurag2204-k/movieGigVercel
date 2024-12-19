7626b1356361ab9b1a48e22063f86f5a48d58c50
Anurag Khobragae <anuragk2204@gmail.com>
2024-12-20 00:13:00 +0530

    changed the cors to *

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,20 +18,24 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // Dynamic CORS configuration to allow both versions of the origin (with and without the trailing slash)
-app.use(cors({
-    origin: (origin, callback) => {
-        const allowedOrigins = [
-            "https://movie-gig-vercel.vercel.app",
-            "https://movie-gig-vercel.vercel.app/", // Allow both versions with and without trailing slash
-        ];
+// app.use(cors({
+//     origin: (origin, callback) => {
+//         const allowedOrigins = [
+//             "https://movie-gig-vercel.vercel.app",
+//             "https://movie-gig-vercel.vercel.app/", // Allow both versions with and without trailing slash
+//         ];
 
-        // Check if the origin is in the allowed origins list or if the origin is undefined (e.g., when running locally)
-        if (allowedOrigins.includes((origin || "").replace(/\/$/, "")) || !origin) {
-            callback(null, origin);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
+//         // Check if the origin is in the allowed origins list or if the origin is undefined (e.g., when running locally)
+//         if (allowedOrigins.includes((origin || "").replace(/\/$/, "")) || !origin) {
+//             callback(null, origin);
+//         } else {
+//             callback(new Error("Not allowed by CORS"));
+//         }
+//     },
+//     credentials: true, // Allow sending cookies
+// }));
+app.use(cors({
+    origin: "*", // Allow all origins
     credentials: true, // Allow sending cookies
 }));
 
